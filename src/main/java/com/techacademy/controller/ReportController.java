ca681e6baa6705b075e11f4e8ad716eb38a58676
hyt07 <leven.tse.leve26550623@gmailcom>
2024-02-17 13:28:18 +0900

    日報一覧のメソッド間の呼び出しを追加

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -1,17 +1,30 @@
 package com.techacademy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.service.ReportService;
 
 @Controller
 @RequestMapping("reports")
 public class ReportController {
 
+    private ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
     // 従業員一覧画面
     @GetMapping
-    public String list() {
-        // 後でクラス間のメソッド呼び出し追加
+    public String list(Model model) {
+
+        model.addAttribute("listSize", reportService.findAll().size());
+        model.addAttribute("reportList", reportService.findAll());
 
         return "reports/list";
     }
