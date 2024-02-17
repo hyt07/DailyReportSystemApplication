ded888b20f69e9bb3c07bc0a0c83b29b01c408a4
hyt07 <leven.tse.leve26550623@gmailcom>
2024-02-18 00:51:51 +0900

    新規登録画面実装

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -1,13 +1,22 @@
 package com.techacademy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
 import com.techacademy.entity.Employee;
+import com.techacademy.entity.Report;
 import com.techacademy.service.ReportService;
 import com.techacademy.service.UserDetail;
 
@@ -36,6 +45,36 @@ public class ReportController {
         model.addAttribute("reportList", reportService.findByEmployee(employee));
 
         return "reports/list";
+    }
+
+    // 日報新規登録画面
+    @GetMapping(value = "/add")
+    public String create(@ModelAttribute Report report) {
+
+        return "reports/new";
+    }
+
+    // 日報新規登録処理
+    @PostMapping(value = "/add")
+    public String add(@Validated Report report, BindingResult res, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+
+        if (res.hasErrors()) {
+            return create(report);
+        }
+
+        Employee employee = userDetail.getEmployee();
+
+        List<Report> reportCheck = reportService.findByEmployeeAndReportDate(employee, report.getReportDate());
+        if (!reportCheck.isEmpty()) {
+            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DATECHECK_ERROR),
+                    ErrorMessage.getErrorValue(ErrorKinds.DATECHECK_ERROR));
+            return create(report);
+        }
+
+        reportService.save(report, employee);
+
+    return "redirect:/reports";
+
     }
 
 }
--- src/main/java/com/techacademy/service/ReportService.java
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -1,10 +1,14 @@
 package com.techacademy.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.techacademy.constants.ErrorKinds;
 import com.techacademy.entity.Employee;
 import com.techacademy.entity.Report;
 import com.techacademy.repository.ReportRepository;
@@ -27,6 +31,24 @@ public class ReportService {
     // 一般ユーザの日報一覧表示処理
     public List<Report> findByEmployee(Employee employee) {
         return reportRepository.findByEmployee(employee);
+    }
+
+ // ログイン中の従業員で同じ日付の日報取得
+    public List<Report> findByEmployeeAndReportDate(Employee employee, LocalDate reportDate) {
+        return reportRepository.findByEmployeeAndReportDate(employee, reportDate);
+    }
+
+    // 日報保存
+    @Transactional
+    public ErrorKinds save(Report report, Employee employee) {
+
+        report.setEmployee(employee);
+        LocalDateTime now = LocalDateTime.now();
+        report.setCreatedAt(now);
+        report.setUpdatedAt(now);
+
+        reportRepository.save(report);
+        return ErrorKinds.SUCCESS;
     }
 
 }
